af8388057ecafdbd756fde49f0f186cba90cd7cd
ccartagena879 <ccartagena879@gmail.com>
2019-04-24 18:13:45 -0500

    bb

--- CoderGirl_MVCMovies/Controllers/MovieRatingController.cs
+++ CoderGirl_MVCMovies/Controllers/MovieRatingController.cs
@@ -8,25 +8,58 @@ namespace CoderGirl_MVCMovies.Controllers
 {
     public class MovieRatingController : Controller
     {
+
+        // Create an GET Action for Create which returns the above template as Content
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return Content(htmlForm, "text/html");
+        }
+
+        private string htmlForm = @"
+            <form method='post' action='/MovieRating/Create'>
+                <input name='movieName' />
+                <select name='rating'>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+                <button type='submit'>Rate</button>
+            </form>";
+
+        [HttpPost]
+        public IActionResult Create(string movieName, string rating)
+        {
+            return RedirectToAction(actionName: nameof(Details), routeValues: new { movieName, rating });
+        }
+
+        [HttpGet]
+        public IActionResult Details(string movieName, string rating)
+        {
+            return Content($"{movieName} has a rating of {rating}");
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-
-        //Create a string html template for a form
-        // with method of post
-        // an input with name "movieName" 
-        // a select with name "rating" and that contains options for 1 through 5
-        // a button with type of submit
-
-        // Create an GET Action for Create which returns the above template as Content
-
         // Create a POST Action for Create which takes...
         // two string parameters which match the names of the input and select names in the template
-        // The method should redirect to a the GET Action for Details. Be sure to pass the parameters as route values.
+        // The method should redirect to a the GET Action for Details. 
+        // Be sure to pass the parameters as route values.
 
         //Create a GET Action for Details
         // Details should take the parameters passed by the POST Action for Create
         // Details should return a string as Content. This string should be in the format "{moveName} has a rating of {rating}"
+
+
+
+
+
+
+
+
     }
 }
